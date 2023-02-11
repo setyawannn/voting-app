d69576c45c082722ad0958ef1b369aea3d4869b9
setyawannn <prayogasetyawan.adi@gmail.com>
2023-02-11 19:12:56 +0700

    set-up next-tailwind

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-green-600 text-3xl font-bold">Be Happy Today!</h1>
+    </div>
+  );
+}
